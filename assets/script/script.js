c594263f7b6b3fdd55bf0f9ced985697583d89a0
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-09-28 09:25:19 -0400

    Simplified the way the function converts the array to a string, and added a function to clear the arrays for reusability

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,30 +1,25 @@
-// Arrays
+// =================== Global Variables ====================================================================================
 const letterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "(", ")", "-", "_", "=", "+", ";", ".", "/", ","];
 
-
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-let pwArr = [];
-let pwObject = {};
-let pw = [];
+var pwArr = [];
+var pw = [];
 var finalPass = ""
-var lastPass = ''
-// Prompt for password length (8-128) || Alert if not valid
-// length
-// Prompt for upper case => passwordArray
-// Prompt for lower case => passwordArray
-// Prompt for numbers => passwordArray
-// Prompt for special characters => passwordArray
+
 
 function generatePassword() {
 
+  // Runs a function to clear arrays for reusability 
+  clearArr()
+
+  // Prompts for desire password length
   let length = window.prompt(`How many characters would you like your password to be? (8-128)`);
   console.log(length);
 
+  // Confirm for upper case letters & Alerts if pw length selection was < 8 || > 128 characters
   if (length >= 8 && length <= 128) {
     var uCase = window.confirm(`Do you want it to contain upper case letters?`);
   } else {
@@ -38,6 +33,7 @@ function generatePassword() {
     console.log(pwArr);
   }
 
+  // Confirm for lower case letters
   var lCase = window.confirm(`Do you want it to contain lower case letters?`)
   console.log(lCase);
   if (lCase == true) {
@@ -45,6 +41,7 @@ function generatePassword() {
     console.log(pwArr)
   }
 
+  // Confirm for numbers
   var num = window.confirm(`Do you want it to contain numbers?`)
   console.log(num);
   if (num == true) {
@@ -52,11 +49,18 @@ function generatePassword() {
     console.log(pwArr)
   }
 
+  // Confirm for special characters
   var spChar = window.confirm(`Do you want it to contail special characters?`)
   console.log(spChar);
   if (spChar == true) {
     pwArr = pwArr.concat(specialCharacters)
     console.log(pwArr);
+  }
+
+  // Alerts if no character types were selected
+  if (uCase == false && lCase == false && num == false && spChar == false) {
+    window.alert(`Atleast one type of character has to be selected!`)
+    return;
   }
   
   // For loop to add characters to [pw]
@@ -65,31 +69,31 @@ function generatePassword() {
   }
   console.log(pw);
 
-  finalPass = ''.concat(pw);
+  // creates new string from pw array
+  finalPass = pw.join('')
   console.log(finalPass);
-
-  lastPass = finalPass.replaceAll(',','')
-  console.log(lastPass)
-
 
 return;
 }
 
+function clearArr() {
+  pw = [];
+  pwArr = [];
+  finalPass = [];
+  console.log(pw, pwArr, finalPass,);
+  return;
+}
 
 
 // Write password to the #password input
 function writePassword() {
-  // var password = 
+  
   generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = lastPass;
+  passwordText.value = finalPass;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-  
